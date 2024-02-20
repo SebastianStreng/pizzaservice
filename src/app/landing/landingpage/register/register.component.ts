f6a20f089712f39857338cb89d6faae1e889280f
Sebastian Streng <51957686+SebastianStreng@users.noreply.github.com>
2024-02-20 14:59:06 +0100

    add register page

--- src/app/landing/landingpage/register/register.component.ts
+++ src/app/landing/landingpage/register/register.component.ts
@@ -8,6 +8,16 @@ import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent implements OnInit {
+  
+  id! : number; 
+  username!: string;
+  password!: string;  
+  firstName!: string;
+  lastName!: string;
+  streetname!: string; 
+  housenumber!: number; 
+  postalCode!: string; 
+  cityname!: string;  
 
   ngOnInit(): void { }
 
