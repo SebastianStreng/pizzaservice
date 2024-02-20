96bf09e8b5424ed43ed40694089c24cf828611b0
Sebastian Streng <51957686+SebastianStreng@users.noreply.github.com>
2024-02-20 14:03:49 +0100

    Update landingpage.component.ts

--- src/app/landing/landingpage/landingpage.component.ts
+++ src/app/landing/landingpage/landingpage.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { SignInComponent } from './signIn/signIn.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-landingpage',
@@ -10,15 +12,16 @@ import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
   //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingpageComponent implements OnInit {
+  ref: DynamicDialogRef | undefined;
 
   constructor(public dialogService: DialogService){}
 
 openSignInDialog (){
-
+  this.ref = this.dialogService.open(SignInComponent, { header: 'Please type in your Username and Password to proceed'});
 }
 
 openRegisterDialog(){
-
+  this.ref = this.dialogService.open(RegisterComponent, { header: 'Please register to order your first custom Pizza'});
 }
 
   ngOnInit(): void { }
