f556c2aff68a8055e836ee732e01ffdcb96600cf
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2024-02-21 15:08:14 +0100

    Update signIn.component.ts

--- src/app/landing/landingpage/signIn/signIn.component.ts
+++ src/app/landing/landingpage/signIn/signIn.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
+import { User } from 'src/app/interfaces/User';
+import { UserService } from 'src/app/services/user-service/user-service';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,8 +13,32 @@ import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 export class SignInComponent implements OnInit {
   username!: string;
   password!: string;
+  users: User[] = [];
+  user!: User;
+
+  error = '';
+  success = '';
+
+  constructor(
+    public dialogService: DialogService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {}
 
   signIn() {}
+
+  getUsers(): void {
+    this.userService.getAll().subscribe({
+      next: (data: User[]) => {
+        this.users = data;
+        this.success = 'Successful retrieval of the users';
+      },
+      error: (err) => {
+        console.error(err);
+        this.error = 'An error occurred while fetching data';
+      },
+      complete: () => console.log('Users fetch complete'),
+    });
+  }
 }
