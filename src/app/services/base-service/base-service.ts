4ef6dba5c3a3e2eb9400c19aeeab6c772631fc34
Sebastian Streng <51957686+SebastianStreng@users.noreply.github.com>
2024-03-01 17:21:37 +0100

    add base service

---
+++ src/app/services/base-service/base-service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Ingredient } from 'src/app/interfaces/ingredient';
+import { base } from 'src/app/interfaces/base';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BaseService {
+  baseUrl = 'http://localhost/api';
+
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    return this.http
+      .get<{ data: base[] }>(`${this.baseUrl}/base`)
+      .pipe(
+        tap((r) => console.log(r)),
+        map((response) => response.data),
+        map((bases) =>
+          bases.map((base) => ({
+            id: base.id,
+            name: base.name,
+            price: Number(base.price),
+            type: base.type
+          }))
+        ),
+        catchError((error: any) => {
+          console.error('Error fetching data:', error);
+          return throwError(() => new Error('Error fetching data'));
+        })
+      );
+  }
+}
